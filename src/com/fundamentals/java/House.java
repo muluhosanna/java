0831e29f1730db59b7522e103e6e4acac9605b3a
mulu <hosannamulu75@gmail.com>
2021-06-10 16:05:24 -0500

    Created House Class

---
+++ src/com/fundamentals/java/House.java
@@ -0,0 +1,14 @@
+package com.fundamentals.java;
+
+public class House {
+
+String foundationType;
+String roofStyle;
+int windowSize;
+String doorColor;
+
+public void doorOpenClose(){
+    System.out.println("The door is open");
+}
+
+}
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("welcome to java");
+        System.out.println("This my first sentence");
+        System.out.println("I am new to java");
+        System.out.println("This java course is cool");
+        System.out.println("I am learning stuff everyday");
+        House myHouse = new House();
+        myHouse.doorOpenClose();
+
+
     }
 }
